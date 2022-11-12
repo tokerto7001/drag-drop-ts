3a0c4bc2d893f08a7a35eca545afb509df63e075
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2022-11-12 21:44:08 +0300

    selecting input elements in the form and add submit event handler to the form

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         // template element for the form
@@ -11,12 +14,32 @@ class ProjectInput {
 
         // import the content of the template element
         const importedNode = document.importNode(this.templateElement.content, true);
+
         // fetches the form element from the imported node which is a template element as a first element child
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input'
+
+        // fetches the input elements from the form element
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        // adds the submit event to the form element
+        this.configure();
         // inserts the selected element into hostElement which is the div with an id of app
         this.attach();
     };
 
+    // handler of the form submission
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value)
+    }
+    // add event listeners to the form element
+    private configure() {
+        // it is crucial to bind the method with the class in order to reach the class object
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
+    }
     // inserts an element inside hostElement
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
