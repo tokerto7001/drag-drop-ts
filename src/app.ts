41b60fd9c268de1759837dae02b6fbbedbe9ab03
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2022-11-13 17:59:31 +0300

    add some control for the people count

--- src/app.ts
+++ src/app.ts
@@ -135,6 +135,14 @@ const projectState = ProjectState.getInstance();
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     private project: Project;
 
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} persons`;
+        };
+    };
+
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
         this.project = project;
@@ -147,7 +155,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
         this.element.querySelector('p')!.textContent = this.project.description;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     };
 }
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
